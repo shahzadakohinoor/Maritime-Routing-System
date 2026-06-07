b968ef0f10cd0da8621fa4d8509e801f776bb379
ALTAB SHAH <bittuempire24@gmai.com>
2026-06-07 19:43:35 +0530

    Production deployment update

--- app.py
+++ app.py
@@ -315,5 +315,5 @@ def download_report():
     return send_file(filename, as_attachment=True)
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+   if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
