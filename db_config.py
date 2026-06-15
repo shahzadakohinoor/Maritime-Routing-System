5f0c9b372191055a93b1d3dfe1c19339fc86d5e1
ALTAB SHAH <bittuempire24@gmai.com>
2026-06-15 21:25:53 +0530

    Week 2 Submission

--- db_config.py
+++ db_config.py
@@ -1,10 +1,10 @@
 import mysql.connector
 
-
 def get_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
         password="",
-        database="maritime_routing"
+        database="maritime_routing",
+        port=3306
     )
