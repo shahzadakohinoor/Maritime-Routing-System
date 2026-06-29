a165be68f5f4a81239984e938aa818c6157dbbeb
ALTAB SHAH <bittuempire24@gmai.com>
2026-06-29 14:25:40 +0530

    feat: initialize week 4 backend API structure

---
+++ db_config.py
@@ -0,0 +1,11 @@
+import os
+import mysql.connector
+
+def get_connection():
+    return mysql.connector.connect(
+        host=os.environ.get("DB_HOST"),
+        user=os.environ.get("DB_USER"),
+        password=os.environ.get("DB_PASSWORD"),
+        database=os.environ.get("DB_NAME"),
+        port=int(os.environ.get("DB_PORT", 3306))
+    )
